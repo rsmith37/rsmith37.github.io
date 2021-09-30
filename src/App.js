f4de564eff640528232f460f6df50dd7ed551a85
Richard Smith <rsmith37@ggc.edu>
2021-09-30 06:56:32 -0400

    basic layout test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const year = new Date().getFullYear();
+
+  // useEffect(() => {
+  //   document.title = "Richard Smith | Portfolio"
+  // }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav style={{display: 'flex', justifyContent: 'space-between', position: 'fixed', top: 0, overflow: 'hidden', width: '100%', zIndex: 99}}>
+        <a href='/' style={{textDecoration: 'none'}}>RICHARD SMITH</a>
+        <div>
+          <a href='#about'>About</a>
+          <a href='#work'>Work</a>
+        </div>
+      </nav>
+
+      <div>
+        <section id="home" style={{height: '100vh'}}> 
+          <div>
+            Home
+          </div>
+        </section>
+        <section id="about" style={{height: '100vh'}}> 
+          <div>
+            About
+          </div>
+        </section>
+      </div>
+      
+      {/* <footer>
+        <p className="text-xs">Designed and Developed by Richard P Smith &copy; {year}</p>
+      </footer> */}
     </div>
   );
 }
-
-export default App;
