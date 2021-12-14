b062f4510c80fe50f2c513e7bb1a472c56e5ceef
Richard Smith <rsmith37@ggc.edu>
2021-12-14 07:50:51 -0500

    added splash page details

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,25 @@
 <script>
+  import {fade} from "svelte/transition";
 </script>
 
 <div class="index-container">
-  Home
+  <p class="home-text">
+    <span in:fade={{delay:500, duration:1000}}>Aspiring software developer.</span> <span in:fade={{delay:1000, duration:2000}}>Avid learner.</span> <span in:fade={{delay:1500, duration:2500}}>Tech enthusiast</span>
+  </p>
 </div>
 
 <style>
 
   .index-container {
-    flex: 1;
+    display: flex;
+    /* flex: 1; */
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+  }
+
+  .home-text {
+    font-size: 3rem;
   }
 
 </style>
