8d8cd172cfe911829913358eb5c037ec6dd84f16
Richard Smith <rsmith37@ggc.edu>
2021-12-19 18:16:58 -0500

    updated home screen to include text and image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,65 @@
 <script>
   import {fade} from "svelte/transition";
+
+  let src = './438.png'
 </script>
 
 <div class="index-container">
   <div class="content-container">
-    <h1 class="welcome">
-      <span in:fade={{duration: 1000}}>Hi </span>
-      <span in:fade={{duration: 1000, delay: 1000}}>and welcome to my website! &#128075;</span>
-    </h1>
+    <div class="text">
+      <h1 in:fade={{duration: 1000}} class="welcome">
+        Hi, I'm Richard, and welcome to my site!
+      </h1>
+      <p>
+        As an aspiring software developer and avid learner, I have created this site to showcase my experience and skills.
+        Thank you for visiting and enjoy!
+      </p>
+    </div>
+    <div class="image">
+      <img src={src} alt="profile pic">
+    </div>
   </div>
 </div>
 
 <style>
 
-  .index-container {
-    /* display: flex; */
-    /* flex: 1; */
-    /* align-items: center;
-    text-align: center;
-    justify-content: center; */
-    width: 75%;
+  .content-container {
+    display: flex;
+    /* justify-content: space-between; */
+    flex-direction: column-reverse;
     margin: auto;
+    align-items: center;
+    text-align: center;
+    width: 90%;  
+  }
+
+  /* @media only screen and (min-width: 425px) {
+    .content-container {
+    }
+  } */
+
+  @media only screen and (min-width: 725px) {
+    .content-container {
+      width: 60%;
+    }
+  }
+
+  /* When the screen is larger than 1302px */
+  @media only screen and (min-width: 1302px) {
+    .content-container {
+      flex-direction: row;
+      text-align: left;
+      justify-content: space-between;
+    }
+
+    .text {
+      margin-right: 3rem;
+    }
+  }
+
+  .image img {
+    vertical-align: middle;
+    border-radius: 50%;
   }
 
 
