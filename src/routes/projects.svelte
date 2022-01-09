0beac109e8fbcb19bd91bc33619b351c2d3de121
Richard Smith <rsmith37@ggc.edu>
2022-01-09 10:27:42 -0500

    added Projects component

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,13 +1,25 @@
 <script>
-
+    import Project from "../components/Project.svelte";
 </script>
 
-<div class="about-container">
-    Work In Progress
+<div class="root-container">
+    <Project proj_name={"Project 1"}
+        proj_desc={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+        techs={["Javascript", "React"]}
+        link={"https://github.com/rsmith37/AngularGoogleMaps"}
+    />
+    <hr />
+    <Project proj_name={"Project 1"}
+        proj_desc={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+        techs={["Javascript", "React", "MongoDB", "NodeJS"]}
+        link={"https://github.com/rsmith37/AngularGoogleMaps"}
+    /> 
 </div>
 
 <style>
-    .about-container {
-        text-align: center;
+    hr {
+        width: 70%;
     }
+
+
 </style>
