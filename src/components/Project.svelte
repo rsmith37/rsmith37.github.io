8a32131ffa6709802f8986622a2e2d1a0af27846
Richard Smith <rsmith37@ggc.edu>
2022-01-09 17:03:40 -0500

    added link to image and font color and size change to skills

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -1,13 +1,13 @@
 <script>
   import { each } from "svelte/internal";
   
-  export let proj_name, proj_desc, techs, link, image;
+  export let proj_name, proj_desc, techs, github_link, image, demo_link;
 </script>
 
 <div class="project-container">
   <div class="title">
     <h3 class="title-name">
-      <a href={link}>
+      <a href={github_link}>
         {proj_name} <span class="arrow">&#8680;</span>
       </a>
     </h3>
@@ -15,8 +15,10 @@
   <div class="content">
     {#if image !== undefined}
      <div class="content-image">
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img src={image} alt="Image Not Availble">
+        <a href={demo_link}>
+          <!-- svelte-ignore a11y-img-redundant-alt -->
+          <img src={image} alt="Image Not Availble">
+        </a>
       </div>
     {/if}
     <div class="content-text">
@@ -49,11 +51,11 @@
     justify-content: center;
   }
 
-  @media only screen and (max-width: 550px) {
+  /* @media only screen and (max-width: 550px) {
     .content-image {
       display: none;
     }
-  }
+  } */
 
   .content-image img {
     max-width: 100%;
@@ -80,12 +82,14 @@
   }
 
   .tech-list-item {
-    padding-left: 5px;
-    padding-right: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
     border-radius: 20px;
     border-color: var(--accent-color);
     border-style: solid;
     border-width: 2px;
+    color: var(--accent-color);
+    font-size: 12px;
   }
 
 </style>
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -4,22 +4,29 @@
 
 <div class="root-container">
     <Project 
-        proj_name={"Project 1"}
+        proj_name={"AngularGoogleMaps"}
         proj_desc={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
         techs={["Javascript", "React"]}
-        link={"https://github.com/rsmith37/AngularGoogleMaps"}
+        github_link={"https://github.com/rsmith37/AngularGoogleMaps"}
         image={'./AngularMaps.png'}
+        demo_link={"https://ggcmaps.wpengine.com/interactive-digital-mapping-in-itec-3860/"}
     />
     <hr />
     <Project 
         proj_name={"Project 1"}
         proj_desc={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
         techs={["Javascript", "React", "MongoDB", "NodeJS"]}
-        link={"https://github.com/rsmith37/AngularGoogleMaps"}
+        github_link={"https://github.com/rsmith37/AngularGoogleMaps"}
     /> 
 </div>
 
 <style>
+
+    .root-container {
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
     hr {
         width: 70%;
     }
