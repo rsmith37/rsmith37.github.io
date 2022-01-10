9378e4f262130b3a0ede9c4ab79eabd100d3bf53
Richard Smith <rsmith37@ggc.edu>
2022-01-10 17:23:04 -0500

    added Shutter Plug project

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -11,6 +11,15 @@
         demo_link={"https://www.youtube.com/watch?v=nFGA5kceqFE&t=8s"}
     /> 
     <hr />
+    <Project 
+        proj_name={"Shutter Plug"}
+        proj_desc={"As a member of a team for Software Development II, worked with external client to design and develop a business focused CRUD web application following the Agile methodology. Shutter Plug's goal was to show off local photographers portfolios and skills, offering profile creation and search, with the intention of connecting them with potential customers."}
+        github_link={"https://github.com/rsmith37/ShutterPlugClone"}
+        techs={["ReactJS", "Redux", "NodeJs/Express", "MongoDB", "HTML", "BootstrapCSS", "Heroku"]}
+        image={'./ShutterPlug.png'}
+        demo_link={"https://www.youtube.com/watch?v=BXEuS-EflOk&feature=youtu.be"}
+    /> 
+    <hr />
     <Project
         proj_name={"GGC Study"}
         proj_desc={"Worked independently as part of Information Technology Research course to create GGC Study, a reactive CRUD web application that allows students to schedule and view study group meetings. Built using the MERN stack, along with Redux for state management and deployed into production using Heroku."}
@@ -21,7 +30,7 @@
     <hr />
     <Project 
         proj_name={"Digital Mapping Project"}
-        proj_desc={"Collaborated with Software Development Technology I team to develop an interactive mapping application using the Angular framework following the Waterfall and Agile methodologies. Utlized Google Maps API to display static location markers that would display information when selected by user."}
+        proj_desc={"Collaborated with Software Development I team to develop an interactive mapping application using the Angular framework following the Waterfall and Agile methodologies. Utlized Google Maps API to display static location markers that would display information when selected by user."}
         techs={["Angular", "TypeScript", "HTML", "CSS", "Google Maps API", "Java"]}
         github_link={"https://github.com/rsmith37/AngularGoogleMaps"}
         image={'./AngularMaps.png'}
